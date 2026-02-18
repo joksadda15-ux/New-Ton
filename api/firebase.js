7232ff6f18127922d9a486185b06768f1a738482
joksadda15-ux <joksadda15@gmail.com>
2026-02-19 05:47:25 +0600

    Create firebase.js

---
+++ api/firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.FB_API_KEY,
+  authDomain: process.env.FB_AUTH_DOMAIN,
+  projectId: process.env.FB_PROJECT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
