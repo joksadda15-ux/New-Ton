bf0c7e9ab31dd2824277afba000be92d9b408d6c
joksadda15-ux <joksadda15@gmail.com>
2026-02-20 16:06:28 +0600

    Fetch user data from Firebase and send messages

--- api/broadcast.js
+++ api/broadcast.js
@@ -2,14 +2,21 @@ export default async function handler(req, res) {
 
   const TOKEN = process.env.BOT_TOKEN;
 
-  const users = [7054894635]; // তোমার chat id
+  const response = await fetch("https://newtube-ton-default-rtdb.firebaseio.com/users.json");
+  const data = await response.json();
 
-  for (const chatId of users) {
+  if (!data) {
+    return res.status(200).json({ success: false, message: "No users found" });
+  }
+
+  const users = Object.values(data);
+
+  for (const user of users) {
     await fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        chat_id: chatId,
+        chat_id: user.chat_id,
         text: "🔥 12hr Reminder!\n\nCome back & earn more TON now!"
       })
     });
